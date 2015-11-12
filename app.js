96ce4b5094e5e7a627a7209972e63d6d23b8f418
Gabe Martinez <mgabemartinez@gmail.com>
2015-11-12 12:15:46 -0800

    Map Site Initial Commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var person = {
+  firstname: 0,
+  lastname: 'Gabe'
+};
+
+console.log(person);
+console.log(person.firstname);
+
+
+
+
+
+
+
+// var a = true;
+
+// if (a) {
+//   console.log('Something is there.');
+// }
+
+// else {
+//   console.log('Nothing is there');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // long running function
+// function waitThreeSeconds() {
+//   var ms = 3000 + new Date().getTime();
+//   while (new Date() < ms){}
+//     console.log('finished function');
+// }
+
+// function clickHandler() {
+//   console.log('click event!');
+// }
+
+// // listen fo the click event
+// document.addEventListener('click', clickHandler);
+
+// waitThreeSeconds();
+// console.log('finished execution');
